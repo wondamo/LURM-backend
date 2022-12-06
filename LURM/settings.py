9d5688a6ddb9156764a7cae6cf499d65997d0d8e
Wonder-David Efe <wonderdavid2@gmail.com>
2022-12-06 09:29:44 +0100

    added allowed hosts

--- LURM/settings.py
+++ LURM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@msv0odj%%pwphtd774ze8v7we!9!-h!*a5u-ajirxii*b%d2%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://lurm-backend.onrender.com/']
 
 
 # Application definition
