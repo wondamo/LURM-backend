8df093858d9ce77f2244713b0568636920cfbaf1
Wonder-David Efe <wonderdavid2@gmail.com>
2022-12-06 12:06:59 +0100

    updated permissions

--- LURM/settings.py
+++ LURM/settings.py
@@ -105,6 +105,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK ={
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ),
+}
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
