c2fd498b9faca7863c3b7bc948f4c1a3bf9b53a0
Wonder-David Efe <wonderdavid2@gmail.com>
2022-12-13 10:33:50 +0100

    edited serializers

--- pastquestion/serializers.py
+++ pastquestion/serializers.py
@@ -6,10 +6,13 @@ from drf_base64.fields import Base64FileField
 
 
 def validate_file(file):
-    if file.content_type not in ['application/pdf', 'image/jpeg', 'image/png', 'image/jpg']:
-        raise serializers.ValidationError(f"{file.content_type} is not a supported format, supported format include: pdf, jpeg, jpg, png")
-    if file.size > 1048576:
-        raise serializers.ValidationError("File size must not be larger than 1MB")
+    try:
+        if file.content_type not in ['application/pdf', 'image/jpeg', 'image/png', 'image/jpg']:
+            raise serializers.ValidationError(f"{file.content_type} is not a supported format, supported format include: pdf, jpeg, jpg, png")
+        if file.size > 1048576:
+            raise serializers.ValidationError("File size must not be larger than 1MB")
+    except AttributeError:
+        raise serializers.ValidationError("The data you submitted is not a supported")
 
 # Create your serializers here
 
