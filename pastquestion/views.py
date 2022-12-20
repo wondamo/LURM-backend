c5c0fe821933423ee6a788748a7c2ac9f29706f1
Wonder-David Efe <wonderdavid2@gmail.com>
2022-12-20 13:02:29 +0100

    added authentication to lurm

--- pastquestion/views.py
+++ pastquestion/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from rest_framework import views, viewsets, filters
+from rest_framework import views, viewsets, filters, permissions
 from .serializers import *
 
 # Create your views here.
 class PastQuestionViewset(viewsets.ModelViewSet):
     serializer_class = PastQuestionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = PastQuestion.objects.all()
     filter_backends = [filters.SearchFilter]
     search_fields = ['courseCode']
