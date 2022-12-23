ccb7b95aec60e1f19c9888ed0918552358f37c0b
Wonder-David Efe <wonderdavid2@gmail.com>
2022-12-23 20:55:47 +0100

    finished authentication

--- pastquestion/views.py
+++ pastquestion/views.py
@@ -10,9 +10,12 @@ class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return response.Response(serializer.data, status=status.HTTP_200_OK)
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            return response.Response(serializer.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return response.Response("User does not exist", status=status.HTTP_400_BAD_REQUEST)
 
 
 class PastQuestionViewset(viewsets.ModelViewSet):
