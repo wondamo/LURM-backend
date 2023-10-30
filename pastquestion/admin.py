97ed5ff68522bb9907ccf8bf4551d3402e481b87
Wonder-David Efe <wonderdavid2@gmail.com>
2023-10-30 13:40:02 +0100

    *

--- pastquestion/admin.py
+++ pastquestion/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    
+    def has_permission(self, request):
+        # Use is_superuser as a condition for admin access
+        return request.user.is_active and request.user.is_superuser
+
+admin.site.register(User, UserAdmin)
 admin.site.register(PastQuestion)
